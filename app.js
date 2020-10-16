e86a114022450e161b9ced06d157c691205304b2
Svetlana Antipova <die-chimaere@yandex.ru>
2020-10-16 16:57:03 +0300

    add helmet and limiter

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const user = require('./routes/user');
 const articles = require('./routes/articles');
 const NotFoundError = require('./errors/NotFoundError');
@@ -14,7 +16,17 @@ const { PORT, DB_ADDRESS } = require('./config');
 
 const app = express();
 
+app.use(helmet());
+
 app.use(cookieParser());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Слишком много запросов с вашего IP, попробуйте повторить попытку позже',
+});
+
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
