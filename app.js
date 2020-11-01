17064f36a4a902cb2bd26f899c989eecd844fa6e
Svetlana Antipova <die-chimaere@yandex.ru>
2020-11-01 14:49:46 +0300

    config cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
 const router = require('./routes');
@@ -9,6 +10,14 @@ const limiter = require('./limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
 const { PORT, DB_ADDRESS } = require('./config');
+
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  preflightContinue: true,
+  maxAge: 600,
+};
 
 const app = express();
 
@@ -18,6 +27,9 @@ app.use(cookieParser());
 app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.options('*', cors(corsOptions));
+app.use(cors(corsOptions));
 
 mongoose.connect(DB_ADDRESS, {
   useNewUrlParser: true,
