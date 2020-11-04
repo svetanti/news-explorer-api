5262afc182861ce0e30fa26612f8bb649cd8bfa4
Svetlana Antipova <die-chimaere@yandex.ru>
2020-11-04 09:48:14 +0300

    rewrite to send token

--- controllres/user.js
+++ controllres/user.js
@@ -35,14 +35,7 @@ module.exports.login = (req, res, next) => {
         JWT_SECRET,
         { expiresIn: '7d' },
       );
-      res
-        .cookie('jwt', token, {
-          maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-          sameSite: 'none',
-          secure: true,
-        })
-        .send({ message: SUCCESS });
+      res.send(token);
     })
     .catch(next);
 };
