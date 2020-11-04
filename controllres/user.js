b1a2de0d46250e65499aba933fa2ac77181ca133
Svetlana Antipova <die-chimaere@yandex.ru>
2020-11-04 09:01:04 +0300

    comment sameSite cookie

--- controllres/user.js
+++ controllres/user.js
@@ -39,7 +39,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: true,
+          // sameSite: true,
         })
         .send({ message: SUCCESS });
     })
